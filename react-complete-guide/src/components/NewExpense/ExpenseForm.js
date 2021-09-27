143232402dd332a52a86c02def3d939e8175f1fd
rbekeris <bekeris.code@gmail.com>
2021-09-27 23:38:23 +0300

    Added child to parent data handling via functions passed as props

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     //We can pass the value entered in the form to the state
     //This helps us to "persist" this value independently from the function's life-cycle
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDatea, setEnteredDate] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     //event is passed by browser automatically on change. Big object with many things inside.
     //The interesting part for us is (event.target.value)
@@ -22,21 +22,41 @@ const ExpenseForm = () => {
         setEnteredDate(event.target.value);
     };
     
+    const submitHandler = (event) => {
+        //We want to prevent the default form submit behavour that reloads the page
+        event.preventDefault();
+
+        //we can refer to all state values and combine them in one object
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        //Upon form submission we use the function passed to this child component
+        //this function is definded in the parent and will handle the data we give it
+        //thus enabling the data flow from child to parent
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+    
     return (
     
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className = "new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} />
+                {/* we can bind the value of the form input field to the state!!!!! by simple value={stateValuename} */}
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
             </div>
             <div className = "new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangehandler}/>
+                <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangehandler}/>
             </div>
             <div className = "new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
             </div>
         </div>
         <div className="new-expense__actions">
--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    //This function will handle the data received from the form submission
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    };
         return (
         <div className="new-expense">
-            <ExpenseForm />
+            {/* our custom components can also incorporate a function that we pass as props to the child component
+            We will use this function in our child function to pass data back to the parent component*/}
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
         );
 };
