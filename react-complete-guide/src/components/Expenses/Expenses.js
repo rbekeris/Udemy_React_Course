5676051212506e127cb7577e2b74d88ef737b00f
rbekeris <bekeris.code@gmail.com>
2021-09-28 21:42:02 +0300

    Assignment 3 complete

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -8,16 +8,27 @@ import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2021')
+    
     const filterChangeHandler = selectedYear =>{
         setFilteredYear(selectedYear);
     };
+
+    //Here we implement filtering for the display
+    //To do that we use .filter method which takes a function applied to each array item
+    //If the function on the array item returns TRUE, the item is kept in the resulting array
+    //Otherwise it is not added to the new array
+    const filteredExpenses = props.items.filter(expense =>{
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
               {/* We use map function to render an ExpenseItem for each value in the
               expense array. In this case "expense" is how we call each single eement in the array*/}
-              {props.items.map(expense => (
+              {filteredExpenses.map( (expense) => (
               <ExpenseItem
+               key ={expense.id}
                title ={expense.title}
                amount={expense.amount}
                date = {expense.date}/>
