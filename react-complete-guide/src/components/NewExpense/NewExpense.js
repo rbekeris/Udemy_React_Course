445a2e504ffc6f4712d84b194320dffa47ab5149
rbekeris <bekeris.code@gmail.com>
2021-09-28 22:48:56 +0300

    added button handler for form

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
     //This function will handle the data received from the form submission
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -10,12 +11,22 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
         return (
         <div className="new-expense">
+            {!isEditing && <button onClick = {startEditingHandler} >  Add New Expense</button>}
             {/* our custom components can also incorporate a function that we pass as props to the child component
             We will use this function in our child function to pass data back to the parent component*/}
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
         </div>
         );
 };
