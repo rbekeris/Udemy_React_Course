16f868bc56755698c991e3e174e4820f071d0cec
rbekeris <bekeris.code@gmail.com>
2021-09-28 20:16:06 +0300

    Added stateful expense list with dinamic array display

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,28 +3,21 @@ import './ExpenseItem.css';
 //We can import other components into components...
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import React, { useState } from 'react';
+import React from 'react';
 
 //First react componenet
 //A React component is just a JS function
 
 function ExpenseItem(props) {
-    const [title, setTitle] = useState(props.title);
-    
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    };
     return (
         //Custom components must be configured to support "extra css" -while in-built components support them out of the box
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
                 {/* This div no longer takes 'props.title' because it is handled by the useState React HOOK*/}
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button> 
+            </div> 
         </Card>
     )
 }
